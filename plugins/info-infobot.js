cbd9936bcbe1af2ce492649ea793978e0663efe5
𝙀𝙧𝙚𝙣𝙭𝙯𝙮 <diegolesimarcelino@gmail.com>
2025-08-03 11:42:32 -0400

    Actualizar info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -15,17 +15,24 @@ let handler = async (m, { conn, usedPrefix }) => {
     let totalStats = Object.values(global.db.data.stats).reduce((total, stat) => total + stat.total, 0)
     let totalf = Object.values(global.plugins).filter((v) => v.help && v.tags).length
 
-    let info = `✿  *Informacion de ${global.botname}*\n\n`
-    info += `✎˚₊· ͟͟͞͞➳❥ *Prefijo* : [  ${usedPrefix}  ]\n`
-    info += `✥˚₊· ͟͟͞͞➳❥ *Total Plugins* : ${totalf}\n`
-    info += `✦˚₊· ͟͟͞͞➳❥ *Comandos Ejecutados* : ${toNum(totalStats)} ( *${totalStats}* )\n\n`
-    info += `*◤ Hosts:*\n`
-    info += `✰˚₊· ͟͟͞͞➳❥ *Plataforma* : ${platform()}\n`
-    info += `✿˚₊· ͟͟͞͞➳❥ *Servidor* : ${hostname()}\n`
-    info += `✧˚₊· ͟͟͞͞➳❥ *RAM* : ${format(totalmem() - freemem())} / ${format(totalmem())}\n`
-    info += `⚘˚₊· ͟͟͞͞➳❥ *Free-RAM* : ${format(freemem())}\n\n`
-    info += `❒ *NodeJS Uso de memoria* :\n`
-    info += `${'```' + Object.keys(process.memoryUsage()).map((key) => `${key}: ${format(process.memoryUsage()[key])}`).join('\n') + '```'}`
+    let info = `
+╭━━━〔 ✿ INFORMACIÓN DE ${global.botname} ✿ 〕━━━╮
+│ ✦ Prefijo » [ ${usedPrefix} ]
+│ ✦ Total Plugins » ${totalf}
+│ ✦ Comandos Ejecutados » ${toNum(totalStats)} ( ${totalStats} )
+│
+│ ◤ HOSTS:
+│ ✦ Plataforma » ${platform()}
+│ ✦ Servidor » ${hostname()}
+│ ✦ RAM » ${format(totalmem() - freemem())} / ${format(totalmem())}
+│ ✦ Free-RAM » ${format(freemem())}
+│
+│ ❒ Uso de Memoria (NodeJS):
+${'```' + Object.keys(process.memoryUsage())
+.map((key) => `${key}: ${format(process.memoryUsage()[key])}`)
+.join('\n') + '```'}
+╰━━━━━━━━━━━━━━━━━━━━━━━╯
+`.trim();
 
     await conn.reply(m.chat, info, fkontak, { contextInfo: { mentionedJid: [owner[0][0] + '@s.whatsapp.net'] } })
 }
