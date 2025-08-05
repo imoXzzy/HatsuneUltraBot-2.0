ba188ca4ad342d2f6750ac3b50354e840beb9a71
𝙀𝙧𝙚𝙣𝙭𝙯𝙮 <diegolesimarcelino@gmail.com>
2025-08-05 15:36:48 -0400

    Actualizar grupo-link.js

--- plugins/grupo-link.js
+++ plugins/grupo-link.js
@@ -2,7 +2,7 @@ var handler = async (m, { conn, args }) => {
 
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, '\t\t✿:･✧ link del grupo ✧･:✿\n\n\v' + link, m, { detectLink: true })
+conn.reply(m.chat, '\t\t❒ 𝗟𝗶𝗻𝗸 𝗗𝗲𝗹 𝗚𝗿𝘂𝗽𝗼 ❒\n\n\v' + link, m, { detectLink: true })
 
 }
 handler.help = ['link']
