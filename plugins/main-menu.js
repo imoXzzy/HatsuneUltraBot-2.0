a3302589b6b45a66151d08106aa56b71521566df
𝙀𝙧𝙚𝙣𝙭𝙯𝙮 <diegolesimarcelino@gmail.com>
2025-08-05 16:32:14 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
-  await m.react('🌟') 
+  await m.react('🏙️') 
 
   let userId = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
   let user = global.db.data.users[userId]
