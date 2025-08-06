252cabf6668641988648de157d90310fa88fbcdf
𝙀𝙧𝙚𝙣𝙭𝙯𝙮 <diegolesimarcelino@gmail.com>
2025-08-06 11:26:24 -0400

    Actualizar grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -9,20 +9,22 @@ const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, co
     throw false;
   }
 
-  const pesan = args.join` `;
-  const oi = `*✎ INFO :* ${pesan}`;
-  let teks = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 📢\n\n ${oi}\n\n╭  ┄ 𝅄 ۪꒰ \`⡞᪲=͟͟͞${botname} ≼᳞ׄ\` ꒱ ۟ 𝅄 ┄\n`;
+  const mensaje = args.join` ` || '¡Hatsune Ultra quiere verlos activos! 🎶';
+  const titulo = `🌸🎤  𝐈𝐍𝐕𝐎𝐂𝐀𝐂𝐈𝐎𝐍 𝐃𝐄 𝐇𝐀𝐓𝐒𝐔𝐍𝐄 𝐔𝐋𝐓𝐑𝐀 🎤🌸`;
+  const subtitulo = `✨♪ ¡Escuchen el llamado de la diva virtual! ♪✨\n⚡ ${mensaje}\n\n🎵 *みんな集まって！一緒に楽しもう！*\n"¡Todos reúnanse! ¡Divirtámonos juntos!"`;
+
+  let teks = `${titulo}\n\n${subtitulo}\n\n╭━━━〔 👥 𝐌𝐈𝐄𝐌𝐁𝐑𝐎𝐒 (${participants.length}) 〕━━━╮\n`;
   for (const mem of participants) {
-    teks += `┊${customEmoji} @${mem.id.split('@')[0]}\n`;
+    teks += `┃ ${customEmoji} @${mem.id.split('@')[0]}\n`;
   }
-  teks += `╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ *${vs}* ୧ ׅ ꒱  ┄  ─ ┄ ⸼`;
+  teks += `╰━━━━━━━━━━━━━━━━━━━━━━╯\n\n🔔 ¡Vamos, despierta ese espíritu vocaloid! 🔔`;
 
   conn.sendMessage(m.chat, { text: teks, mentions: participants.map((a) => a.id) });
 };
 
 handler.help = ['todos *<mensaje opcional>*'];
 handler.tags = ['group'];
-handler.command = ['todos', 'invocar', 'tagall']
+handler.command = ['todos', 'invocar', 'tagall'];
 handler.admin = true;
 handler.group = true;
 
